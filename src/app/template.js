195b02fcfe317801caa4762975203e2e8471219a
Faisal Abrar <erfaisalabrar@gmail.com>
2025-06-29 13:53:25 +0530

    added home page route on dashboard section of sidebar

--- src/app/template.js
+++ src/app/template.js
@@ -14,7 +14,7 @@ export default function SidebarDemo({ children }) {
   const links = [
     {
       label: "Dashboard",
-      href: "#",
+      href: "/",
       icon: (
         <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
       ),
